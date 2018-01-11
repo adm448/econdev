9b532d57f7a88d1a9c6fdaef1d856ef52499aa72
Adam Murray <adm448@student.hks.harvard.edu>
2018-01-11 14:32:06 -0500

    link ITU data to choropleth template

--- scripts/vis1_global_internet_subscriptions.js
+++ scripts/vis1_global_internet_subscriptions.js
@@ -1,17 +1,17 @@
 var format = function(d) {
-    d = d / 1000000;
-    return d3.format(',.02f')(d) + 'M';
+    d = d;
+    return d3.format(',.02f')(d) + 'per 100';
 }
 
 var map = d3.geomap.choropleth()
     .geofile('/data/countries.json')
     .colors(colorbrewer.YlGnBu[9])
-    .column('YR2010')
+    .column('2016')
     .format(format)
     .legend(true)
     .unitId('Country Code');
 
-d3.csv('/data/testdata.csv', function(error, data) {
+d3.csv('/data/Fixed_broadband_2000-2016_with_country_codes.csv', function(error, data) {
     d3.select('#map')
         .datum(data)
         .call(map.draw, map);
