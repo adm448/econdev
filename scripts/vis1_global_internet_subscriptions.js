5328dd2f49e5ca2870bc982a9afccb1556d440d6
Adam Murray <adm448@student.hks.harvard.edu>
2018-01-12 14:47:01 -0500

    tweak file paths to display on web

--- scripts/vis1_global_internet_subscriptions.js
+++ scripts/vis1_global_internet_subscriptions.js
@@ -4,14 +4,14 @@ var format = function(d) {
 }
 
 var map = d3.geomap.choropleth()
-    .geofile('/data/countries.json')
+    .geofile('data/countries.json')
     .colors(colorbrewer.Blues[9])
     .column('2016')
     .format(format)
     .legend(true)
     .unitId('Country Code');
 
-d3.csv('/data/Fixed_broadband_2000-2016_with_country_codes.csv', function(error, data) {
+d3.csv('data/Fixed_broadband_2000-2016_with_country_codes.csv', function(error, data) {
     d3.select('#map')
         .datum(data)
         .call(map.draw, map);
