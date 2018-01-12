71d031a765f6321e21dd3eacbaaf38fc5eb72c51
Adam Murray <adm448@student.hks.harvard.edu>
2018-01-12 15:44:45 -0500

    insert second map with 2006 data

--- scripts/vis1_global_internet_subscriptions_2006.js
+++ scripts/vis1_global_internet_subscriptions_2006.js
@@ -3,17 +3,16 @@ var format = function(d) {
     return d3.format(',.02f')(d) + ' per 100 habitants';
 }
 
-var map = d3.geomap.choropleth()
+var map2k6 = d3.geomap.choropleth()
     .geofile('data/countries.json')
     .colors(colorbrewer.Blues[9])
-    .column('2016')
+    .column('2006')
     .format(format)
     .legend(true)
     .unitId('Country Code');
 
 d3.csv('data/Fixed_broadband_2000-2016_with_country_codes.csv', function(error, data) {
-    d3.select('#map')
+    d3.select('#map2006')
         .datum(data)
-        .call(map.draw, map);
-
+        .call(map2k6.draw, map2k6);
 });
